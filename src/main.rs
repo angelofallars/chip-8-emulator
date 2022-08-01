42176c29ad7ed4c6df49fe215e85f9b65f6cb17f
Angelo Fallaria <ba.fallaria@gmail.com>
2022-08-02 04:34:46 +0800

    fix: make 0x9 register accurate again

--- src/main.rs
+++ src/main.rs
@@ -95,7 +95,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
                 }
             }
             0x9000 => {
-                if register[x] == register[y] {
+                if register[x] != register[y] {
                     program_counter += 2;
                 }
             }
